31e9e27f690d4eaa611d4f9b30e855944bedbb28
Junming Zheng <lucidstudio@gmail.com>
2017-10-31 12:31:37 +0800

    chapter3.1 done

--- hello.py
+++ hello.py
@@ -17,7 +17,21 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    values=[1,2,2,3,4,5,4,6,7]
+    return render_template('user.html',name=name,values = values)
+
+@app.route('/test/<selector>')
+def testVar(selector):
+    mydict = dict()
+    mydict['a']='t'
+    mydict['t']='a'
+    mylist = [1,2,3,4,5]
+    key = 'a'
+    myintvar = int(selector)
+    class myobj:
+        def somemethod():
+            return "test some method"
+    return render_template('test.html',mydict=mydict,key=key,mylist=mylist,myintvar=myintvar,myobj=myobj)
 
 if __name__ == '__main__':
     app.run(debug=True)
